242b729ab673e63531fda10941f46b69f1f589e7
Sakti Abdullah Shidiq <specialhisaki@gmail.com>
2023-12-05 23:01:53 +0700

    fix logo and maybe experiment it later

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -19,12 +19,12 @@ const Experience = () => {
           {experience.map((list) => (
             <div
               key={list.name}
-              className="flex flex-col items-center text-center space-y-1 bg-red-ruby text-almost-white max-w-lg p-4"
+              className="flex flex-col items-center text-center space-y-1 bg-red-ruby text-almost-white max-w-lg p-4 rounded-tl-[4rem]"
             >
               <Image alt={list.name} src={list.image} width={64} height={64} />
               <h2 className="bold-16 sm:bold-20">{list.name}</h2>
               <h4 className="font-regular text-xs">
-                {DateExperience(list.start)} -{" "}
+                {DateExperience(list.start)} - {" "}
                 {list.end ? DateExperience(list.end) : "Present"}
               </h4>
               <h3 className="medium-14 sm:medium-16">{list.jobs}</h3>
