7ffcbf3376bee758bfe52ad83dc8dc0542673052
Sakti Abdullah Shidiq <specialhisaki@gmail.com>
2024-02-01 11:19:26 +0700

    Adding vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Unbounded, Montserrat } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const montserrat = Montserrat({
   subsets: ["latin"],
@@ -22,12 +23,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${montserrat.className} relative bg-almost-white mx-auto`}
-      >
+        className={`${montserrat.className} relative bg-almost-white mx-auto`}>
         <Navbar />
         {children}
       </body>
       <SpeedInsights />
+      <Analytics />
     </html>
   );
 }
